9dadcddc5e68d06ccfddfe89805553a0a568f789
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2023-05-29 21:37:33 +0600

    [Done] ES Lint Stage Checked

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-app.use(cors())
+          app.use(cors())
 
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Hello From World');
 })
 
 export default app
